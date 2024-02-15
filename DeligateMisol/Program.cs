7b317775640af430e191890bfb948efe58fb9931
Humoyun Ochilov Ravshan o'g'li <43071439+dotNet-developer1995@users.noreply.github.com>
2024-02-15 12:09:31 +0500

    Add project files.

---
+++ DeligateMisol/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace DelegateMisol
+{
+    public class Program
+    {
+        // signature - methodning input va output tipini bildiradi
+        public delegate int Calculation(int a, int b);
+        public static void Main(string[] args)
+        {
+            Calculation calculate = new Calculation(Multiplication);
+            Console.WriteLine(calculate.Invoke(6, 7));
+
+            //Calculation calculate = Multiplication;
+            //Console.writeLine(calculate(6, 7));
+        }
+        private static int Multiplication(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
